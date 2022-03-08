916325ba6fed6f4297de70d7d451dcb4ba5b270f
OksanaSha <oksanashatalova@bk.ru>
2022-03-08 22:58:58 +0300

    change models m2m

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 
 from .models import Student, Teacher
 
+class GroupInline(admin.TabularInline):
+    model = Student.teacher.through
+    extra = 1
+
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'group']
+    inlines = [GroupInline]
+
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'subject']
+    inlines = [GroupInline]
