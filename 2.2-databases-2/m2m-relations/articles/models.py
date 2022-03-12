98c6cc2a88db3759ed28057e23c7523c3f18e4a0
OksanaSha <oksanashatalova@bk.ru>
2022-03-12 15:38:31 +0300

    add Tag, Scope, main tag, ordering in models, formset

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -3,14 +3,39 @@ from django.db import models
 
 class Article(models.Model):
 
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
+    title = models.CharField(max_length=256, verbose_name='Название', unique=True)
+    text = models.TextField(verbose_name='Текст', unique=True)
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Тэг', unique=True)
+    articles = models.ManyToManyField(Article, related_name='tag', through='Scope')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ['-is_main', 'tag__name']
