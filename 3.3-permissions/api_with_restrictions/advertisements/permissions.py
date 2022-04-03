7a049c4f44d3d5318878fe1ff49e579ccae21c09
OksanaSha <oksanashatalova@bk.ru>
2022-04-03 23:33:12 +0300

    Add DRAFT, get_queryset

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -1,7 +1,12 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAuthenticated
+
 
 class IsOwnerOrReadOnly(BasePermission):
+
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
+            if obj.status == 'DRAFT':
+                return request.user == obj.creator
             return True
-        return request.user == obj.creator
+        return request.user == obj.creator or request.user.is_staff
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
@@ -14,19 +14,26 @@ class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
 
     def get_queryset(self):
+        queryset = Advertisement.objects.filter(status='OPEN')
         creator = self.request.query_params.get('creator', None)
         if creator:
-            queryset = Advertisement.objects.all()
-        else:
-            queryset = Advertisement.objects.filter(status='OPEN')
+            queryset_closed = Advertisement.objects.filter(status='CLOSED')
+            queryset |= queryset_closed
+        if self.request.user.is_authenticated:
+            queryset_draft = Advertisement.objects.filter(creator=self.request.user, status='DRAFT')
+            queryset |= queryset_draft
         return queryset
 
-    queryset = Advertisement.objects.all()
+    # queryset = Advertisement.objects.exclude(status='DRAFT')
     serializer_class = AdvertisementSerializer
     filterset_class = AdvertisementFilter
     filter_backends = [DjangoFilterBackend]
     permission_classes = [IsOwnerOrReadOnly]
 
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        return [IsOwnerOrReadOnly()]
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
